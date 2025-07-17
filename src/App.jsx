83ba58338bd4f0a7b5aaabdd30d429cc194bea84
Muazzam Hussain <muazzamsaeed09@gmail.com>
2025-07-17 08:52:25 +0500

    whatsapp feature added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
+      
+      <a
+        href="https://wa.me/+971505937099" //Floating whatsapp icon
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 bg-green-500 rounded-full shadow-lg p-4 z-50 hover:scale-110 transition-transform duration-300"
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/124/124034.png"
+          alt="WhatsApp"
+          className="w-6 h-6"
+        />
+      </a>
       {/* Nav, header, etc. */}
       <Outlet /> {/* Renders nested route components here */}
     </div>
